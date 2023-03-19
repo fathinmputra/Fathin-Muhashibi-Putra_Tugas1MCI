bb4c55bdaf01f4637f275bbae89092b2968c192e
Fathin Muhashibi Putra <fathinmp11@gmail.com>
2023-03-19 15:46:06 +0700

    Add no2 solution for JavaScript Problem

---
+++ JavaScript/no2/fungsiAritmatika.js
@@ -0,0 +1,25 @@
+// File pertama berisikan fungsi aritmatika sederhana seperti penjumlahan, pengurangan, perkalian, pembagian
+
+function pertambahan(a, b) {
+    return a + b;
+  }
+  
+  function pengurangan(a, b) {
+    return a - b;
+  }
+  
+  function perkalian(a, b) {
+    return a * b;
+  }
+  
+  function pembagian(a, b) {
+    return a / b;
+  }
+  
+  module.exports = {
+    pertambahan,
+    pengurangan,
+    perkalian,
+    pembagian
+  };
+  
---
+++ JavaScript/no2/perhitungan.js
@@ -0,0 +1,8 @@
+// File kedua tampilkan hasil perhitungan berikut menggunakan modul dari file pertama : 4000 / 2 + 25 - 2
+
+const { pertambahan, pengurangan, perkalian, pembagian } = require('./fungsiAritmatika.js');
+
+// 4000 / 2 + 25 - 2
+// (4000 / 2) + (25 - 2)
+const hasil = pertambahan(pembagian(4000, 2), pengurangan(25, 2));
+console.log(hasil); //2023
